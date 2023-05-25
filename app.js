eef039264417a2562f3bed2832a1d48c74b01bc3
Austin Lovold <austin@lovolds.com>
2023-05-25 11:45:10 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum (arr1) {
+    let sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        sum = sum + arr1[i];
+    }
+    return sum;
+}
+
+
+console.log(arraySum(numbers));
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
